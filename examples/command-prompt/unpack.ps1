162c5de21a2c82d9d2bb9e9446861bf09c38840d
Scott W Harden <swharden@gmail.com>
2026-05-01 23:42:27 -0400

    update powershell extraction to support payloads with exit code

--- examples/command-prompt/unpack.ps1
+++ examples/command-prompt/unpack.ps1
@@ -1,12 +1,11 @@
 $data = [IO.File]::ReadAllBytes("image_with_payload.jpg")
 
-# find last FF D9 (end of JPEG)
-for ($i = $data.Length - 2; $i -ge 0; $i--) {
+# find first FF D9 (end of the carrier JPEG)
+for ($i = 0; $i -lt $data.Length - 1; $i++) {
     if ($data[$i] -eq 0xFF -and $data[$i + 1] -eq 0xD9) {
         $start = $i + 2
         break
     }
 }
 
-# write everything after it
-[IO.File]::WriteAllBytes("extracted_payload.bin", $data[$start..($data.Length - 1)])
+[IO.File]::WriteAllBytes("extracted_payload.mp3", $data[$start..($data.Length - 1)])
